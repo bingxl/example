98740efda1273a418148b38e13837e7ced5b7c8c
bingxl <2282152858@qq.com>
2018-09-13 12:00:46 +0800

    fix: fix request

--- http/server/request.js
+++ http/server/request.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
 const http = require('http');
-const hostname = 'dev.wemiracle.com';
-const path = '/Uploads/WxaQrcode/1535526274.png';
+const hostname = 'fm.shiyunjj.com'; // 主站 m.mmjpg.com
+const path = 'large/2018/1460.jpg';
 let options = {
   port: 80,
   hostname,
   method: 'GET',
-  path
+  path,
+  headers: {
+    "Referer": "http://m.mmjpg.com/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+
+  }
 };
 
-const request = http.request(options, (res) => {
+const request = http.request(options, (res) => {``
   console.log('STATUS: ', res.statusCode);
   console.log('HEADERS: ', JSON.stringify(res.headers));
-  res.setEncoding('utf8'); // 设置了encoding 后data事件的回调函数参数变成字符串
+  res.pipe(fs.WriteStream(__dirname + '/test.jpg'));
+  // res.setEncoding('utf8'); // 设置了encoding 后data事件的回调函数参数变成字符串
   // const stream = fs.createWriteStream('./test.png');
-  let buffer = Buffer.from('');
+  // let buffer = Buffer.from('');
   res.on('data', (chunk) => {
     // stream.write(chunk);
-    buffer = Buffer.concat([buffer, Buffer.from(chunk)]);
   }).on('end', () => {
-    fs.writeFileSync('./test.txt', buffer.toString('base64'));
     // console.log(buffer.toString('base64'));
     console.log('接受数据完毕');
   });
 });
+console.log(request.headers);
 request.end();
